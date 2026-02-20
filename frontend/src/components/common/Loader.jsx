910bdb9e206160c8ba0316abfb6e647bf791526c
Adeela Azeez <adeelaazeez9@gmail.com>
2026-02-20 23:08:22 +0530

    Added updated SRS document and made changes to the frontend components and styles. Removed RoleBasedRoute component and some CSS files related to dashboard, forms, and tables.

---
+++ frontend/src/components/common/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+    return (
+        <div className="flex justify-center items-center p-8">
+            <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-indigo-600"></div>
+        </div>
+    );
+}
+
+export default Loader;
