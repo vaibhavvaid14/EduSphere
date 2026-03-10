8ec0be460a45116d37cb9be792d95c26fdb01459
Vaibhav Vaid <142966207+vaibhavvaid14@users.noreply.github.com>
2026-03-11 00:23:15 +0530

    Added GiveFeedback component.jsx

---
+++ frontend/src/pages/student/GiveFeedback.jsx
@@ -0,0 +1,11 @@
+import FeedbackForm from "../../components/student/FeedbackForm";
+
+const GiveFeedback = () => {
+  return (
+    <div className="p-6">
+      <FeedbackForm />
+    </div>
+  );
+};
+
+export default GiveFeedback;
